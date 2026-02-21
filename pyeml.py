97c0246633fbbe5b6663d89a03fb5c07b6507a19
Rob Helm <rhelm@directionsonmicrosoft.com>
2026-02-21 14:01:27 -0800

    Started html->text case

---
+++ pyeml.py
@@ -0,0 +1,167 @@
+# pyeml.py
+# Extract customer questions from Interests .eml files
+# Uses python base email parser
+
+import email.parser
+import email
+from html.parser import HTMLParser
+import os, os.path
+# import pandas as pd
+# import datetime
+# import json
+# import eml_parser
+# import extract_msg as emsg
+from pprint import pprint as pp
+import re
+
+
+class InterestsEmlParser()
+    inpath_default = r"C:\Users\Rob\script\data\botdata_files\interests"
+    outpath_default  = r"C:\Users\Rob\script\data\botdata_files\interests_out.txt"
+    clean_pattern = re.compile(r'\s\s\s*')
+
+    def __init__(self):
+        self.parser = email.parser.BytesParser(policy=email.policy.default)
+        self.html_parser
+        
+    def combine_message_files(self, inpath=inpath_default, outpath=outpath_default):
+        result = concat_files(inpath, list([]))
+
+        with open(outpath, mode='w', encoding='utf-8') as outf:
+            pp(result, stream=outf)
+        outf.close()
+        return(result)
+
+    
+def concat_files(inpath, result):
+    dbg_limit = 1
+    for filename in os.scandir(inpath):
+        extension = message_file_type(filename)
+        if extension == '.eml':
+            dbg_limit = dbg_limit - 1
+            if dbg_limit < 0:
+                break
+            print(f'is {extension} file') # DBG
+            result = self.append_eml(filename.path, result)
+            '''
+        elif extension == '.msg':
+            print(f'is msg file')
+            result = append_msg(filename.path, extension, result)
+            '''
+        else:
+            print('is not message file')   #DBG 
+        print(f'result contains {len(result)} messages') # DBG
+    return(result)
+
+
+# Convert html to plain text and clean out extra line breaks and whitespace
+
+    def clean_message_body(self, body_text):
+        body = re.sub(self.pattern, self.cleaner, body_text)
+        return(body)
+
+    def cleaner(matched):
+        if matched.group(1):
+            return('\n')
+        else:
+            return(' ')
+
+    # append .eml message body_txt to question file
+
+    def append_eml(self, message_path, result):
+        print(f'append_eml {message_path}') #DBG
+        with open(message_path, 'rb') as message_file:
+            msg = self.parser.parse(message_file)
+        message_file.close()
+        print(f'msg read')
+        body_text = self.get_body(msg)
+        print(f'append body_text: {len(body_text)} chars')
+        result.append(body_text)
+        return(result)
+
+    # Get the text body from an email
+    def get_body(self, msg):
+        # If message is not MIME multipart, get text from base message body
+        if not msg.is_multipart():
+            print('not multipart')
+            body_text = self.get_msg_body(msg)
+        else:
+            print('multipart')
+            part = msg.get_body(preferencelist=('html', 'plain'))
+            if part:
+                print('part found')
+                body_text = self.get_msg_body(part) 
+            else:
+                body_text = ''
+        return(body_text)
+
+    def get_msg_body(self, msg):
+        content = msg.get_content()
+        maintype = msg.get_content_maintype()
+        subtype = msg.get_content_subtype()
+        print(f'content_type: {maintype}/{subtype}')
+        if maintype == 'text':
+            if subtype == 'html':
+                body_text = self.html_text(content)
+                print('converted html to text')
+            elif subtype == 'plain':
+                body_text = content
+            else:
+                body_text = ''
+        else:
+            body_text = ''
+        return(body_text)
+
+        
+
+    # Message is multipart find text parts (sample code, stopped here 2026-02-21 0830)
+    
+    for part in msg.walk():
+        part_type = part.get_content_type()
+        part.get('Content-Disposition') is None
+        if part_type == 'text/plain' or and :
+            # Return the decoded payload (the email body text)
+            return part.get_payload(decode=True).decode(part.get_content_charset() or 'utf-8')
+
+    
+'''
+def append_msg(path, extension, result):
+    try:
+        message_file = emsg.openMsg(path)
+        print(f'raw MSG file read')
+        msg = dict({})
+        msg['body'] = message_file.body #clean_message_body()
+        msg['subject'] = (message_file.subject)
+        msg['receivedTime'] = (message_file.receivedTime)
+        msg['rtfBody'] = (message_file.rtfBody)
+        result.append(msg)
+    finally:
+        message_file.close()
+    return(result)
+'''
+# Get extension from filename
+
+def message_file_type(filename):
+    path = filename.path
+    if os.path.isfile(path):
+        (base,extension) = os.path.splitext(path)
+        if extension == '.msg' or extension == '.eml':
+            print(f'message_file_type: {extension}')
+            return(extension)
+        else:
+            print('message_file_type: not a message file type')
+            return('')
+    else:
+        print('message_file_type: not a file')
+        return('')
+
+
+    
+class TextParser(HTMLParser):
+    def __init__(self)
+    self.stack = list([])
+
+
+
+if __name__ == '__main__':
+    result = combine_message_files()
