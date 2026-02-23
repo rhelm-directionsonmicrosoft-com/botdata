e5904a620a29ac7fae62d807f687faff0ec3ae84
Rob Helm Directions <rhelm@directionsonmicrosoft.com>
2026-02-22 18:15:34 -0800

    Working on message parse for eml

---
+++ tmp.py
@@ -0,0 +1,18 @@
+msg = r'''Questions in Query: *** please provide content and a short response based on 
+ this customer question ** Hi, Im looking for more detailed information 
+ about the Cloud Solution Provider (CSP) program and how it compares to the 
+ Enterprise Agreement (EA). Two key concerns I’ve come across—but haven’t 
+ been able to find clear answers to—are: • The difference in support, where 
+ CSP relies on the partner rather than Microsoft directly. • Challenges when 
+ switching from one CSP partner to another, particularly in Azure, including 
+ issues related to subscription ownership. I’ve already found some 
+ documentation comparing the Microsoft Customer Agreement (MCA) and EA, but 
+ I’m specifically looking for something that outlines the pros and cons of 
+ CSP in comparison to both EA and MCA. Do you have any material or guidance 
+ you could share on this? Best regards, Stig -- Stig Sjöberg 
+ Infrastrukturarkitekt Tel direkt + 46 8 655 90 58 Tel mobil + 46 70 509 41 
+ 35 Tel växel + 46 771 787 000 www.sveaskog.se\r\n
+   ________________________________\r\n
+ <Draft Customer Answer>\r\n
+ \r\n'
+ '''
